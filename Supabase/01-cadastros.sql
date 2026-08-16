7468ffd476a47cfcbd5fe00ea1b066f36f2ff865
LogisticaCocoLegal <logistica@cocolegal.com.br>
2026-08-16 00:17:51 -0300

    Create 01-cadastros.sql

---
+++ Supabase/01-cadastros.sql
@@ -0,0 +1,39 @@
+/*
+==================================================
+PROJETO: COCO LEGAL LOGÍSTICA
+ARQUIVO: 01-cadastros.sql
+
+TABELAS MESTRES
+
+- tb_bases
+- tb_colaboradores
+- tb_clientes
+- tb_usuarios
+- tb_fornecedores
+- tb_importacoes
+
+==================================================
+*/
+
+-- Base operacional
+
+-- tb_base*
+
+-- Colaboradores
+
+-- tb_colabora*ores
+
+-- Clientes
+
+-- tb_clientes
+*-- Usuários
+
+-- tb_usuarios
+
+-- Fo*necedores
+
+-- tb_fornecedores
+
+-- *istórico de importações
+
+-- tb_imp*rtacoes

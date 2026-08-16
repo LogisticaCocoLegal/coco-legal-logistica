e713485bbe2bc924feef9a522c824eae8f698e94
LogisticaCocoLegal <logistica@cocolegal.com.br>
2026-08-16 00:34:57 -0300

    Create 06-views.sql

---
+++ Supabase/06-views.sql
@@ -0,0 +1,30 @@
+/*
+===========*==================================*===
+ARQUIVO: 06-views.sql
+========*==================================*======
+
+VIEWS:
+
+- vw_dashboard_ceo
+- vw_planejado_executado
+- vw_cancelados
+- vw_encaixes
+- vw_transferidos
+- vw_recebidos
+- vw_ocorrencias
+- vw_premiacoes
+- vw_custos_terceiros
+- vw_produtividade
+
+OBJETIVOS:
+
+- Dashboard CEO
+- Indicadores Operacionais
+- Indicadores de Qualidade
+- Indicadores Financeiros
+- Indicadores de Premiação
+
+STATUS:
+
+EM MODELAGEM
+*/

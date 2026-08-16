10555c19ffa7c96170a53d72ea1f8fc069de8e1d
LogisticaCocoLegal <logistica@cocolegal.com.br>
2026-08-16 00:34:38 -0300

    Add header comments to 05-premiacao.sql
    Added header comments detailing the file purpose and objectives.

---
+++ Supabase/05-premiacao.sql
@@ -0,0 +1,22 @@
+/*
+========*==================================*======
+ARQUIVO: 05-premiacao.sql
+=*==================================*=============
+
+TABELAS:
+
+- tb_prem*acao_diaria
+- tb_regras_premiacao
+*OBJETIVOS:
+
+- Premiação de Motoboy*
+- Premiação de Motoristas
+- Premi*ção de Ajudantes
+- Premiação de Ci*listas
+- Premiação de Logística
+- *remiação de Expedição
+
+STATUS:
+
+EM*MODELAGEM
+*/

6be6f71b51523144c67ad8ea28cae47c0bf1dcbf
LogisticaCocoLegal <logistica@cocolegal.com.br>
2026-08-16 00:33:48 -0300

    Create 03-ocorrencias.sql

---
+++ Supabase/03-ocorrencias.sql
@@ -0,0 +1,24 @@
+/*
+==================================================
+ARQUIVO: 03-ocorrencias.sql
+==================================================
+
+TABELAS:
+
+- tb_ocorrencias
+- tb_ocorrencias_colaborador
+- tb_historico_clientes
+
+OBJETIVOS:
+
+- Reclamações de clientes
+- Tratativas
+- Faltas
+- Atestados
+- Atrasos
+- Histórico de alterações cadastrais
+
+STATUS:
+
+EM MODELAGEM
+*/

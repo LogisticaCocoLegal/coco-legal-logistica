c35f24a292b80b36b3d7a866c088ea9be8112a14
LogisticaCocoLegal <logistica@cocolegal.com.br>
2026-08-16 00:24:06 -0300

    Create 02-operacao.sql for operational tables
    Add SQL file for operational tables documentation.

---
+++ Supabase/02-operacao.sql
@@ -0,0 +1,15 @@
+/*
+==================================================
+ARQUIVO: 02-operacao.sql
+
+TABELAS OPERACIONAIS
+
+- tb_clientes_planejados
+- tb_clientes_executados
+- tb_planejamento_rotas
+- tb_execucao_rotas
+- tb_movimentacao_base
+- tb_observacoes
+
+==================================================
+*/
